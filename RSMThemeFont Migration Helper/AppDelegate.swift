1da742f7641227b3e386af32dd3332e96c21abfc
Sergey Popov <spopov@readdle.com>
2020-01-14 20:55:14 +0200

    Initial Commit

---
+++ RSMThemeFont Migration Helper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RSMThemeFont Migration Helper
+//
+//  Created by Sergi on 14/01/2020.
+//  Copyright © 2020 Readdle. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RSMThemeFont Migration Helper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RSMThemeFont Migration Helper
+//
+//  Created by Sergi on 14/01/2020.
+//  Copyright © 2020 Readdle. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
